368f3c184b413a876e3125bf23073644945bb337
CloudERPng <38726533+CloudERPng@users.noreply.github.com>
2021-01-08 12:37:01 +0100

    Update install.py
    update
    Remove Forum etc

--- erpnext/setup/install.py
+++ erpnext/setup/install.py
@@ -124,19 +124,7 @@ def add_standard_navbar_items():
 		{
 			'item_label': 'Documentation',
 			'item_type': 'Route',
-			'route': 'https://erpnext.com/docs/user/manual',
-			'is_standard': 1
-		},
-		{
-			'item_label': 'User Forum',
-			'item_type': 'Route',
-			'route': 'https://discuss.erpnext.com',
-			'is_standard': 1
-		},
-		{
-			'item_label': 'Report an Issue',
-			'item_type': 'Route',
-			'route': 'https://github.com/frappe/erpnext/issues',
+			'route': 'https://academy.clouderp.one/',
 			'is_standard': 1
 		}
 	]
